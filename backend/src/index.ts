10267fa53d76adf87705be54fd87d57c5f734d6f
Ashita <ashitam2407@gmail.com>
2025-02-02 16:04:36 +0530

    built a basic broadcasr where if multiple users connect message by one user can be seen by all

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,15 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 let CountUser = 0;
-
+let allSockets = [];
 wss.on("connection", (socket) => {
   CountUser = CountUser + 1;
+  allSockets.push(socket);
   console.log("user connected" + CountUser);
 
   socket.on("message", (event) => {
-    console.log("message recieved" + event.toString());
+    console.log("message recieved " + event.toString());
+
+    allSockets.forEach((a) => a.send(event.toString() + " sent from server"));
   });
 });
